a47239c8bda34cd2392edb8f49a349eb05898ac6
Chance093 <chance.dev093@gmail.com>
2022-11-09 15:33:07 -0800

    Add function to request projects

--- src/dom.js
+++ src/dom.js
@@ -1,14 +1,19 @@
 const container = document.querySelector('.project-folder-list');
+const pfButton = document.querySelector('.pf-button');
 
 function displayFolders(folders) {
     container.innerHTML = '';
     const list = document.createElement('ul');
-    folders.forEach(folder => {
+    folders.forEach((folder, index) => {
         const projFolder = document.createElement('li');
         const button = document.createElement('button');
         const div = document.createElement('div');
         const projectList = document.createElement('ul');
         projectList.classList.add('project-list');
+        projectList.setAttribute('data-pf-index', index);
+        button.classList.add('pf-button');
+        button.setAttribute('data-button-index', index);
+        button.addEventListener('click', requestProjects);
         div.textContent = folder.name;
         button.textContent = '+';
         div.appendChild(button);
@@ -19,6 +24,10 @@ function displayFolders(folders) {
     container.appendChild(list);
 }
 
+function requestProjects() {
+    const modal = document.querySelector('.modal-container');
+    modal.classList.add('show');
+}
 
 
 export { displayFolders };
