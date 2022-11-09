8455c47fbc4eaa06cef39b85516a24bc0ffb7277
Chance093 <chance.dev093@gmail.com>
2022-11-08 22:44:54 -0800

    Add functions to add todo to an array

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import './style.css';
 
-console.log('Yerrrr World');
+
+const input = document.querySelector('#test');
+const button = document.querySelector('.testbutton');
+const todos = [];
+
+function Todo(task, date) {
+    let dateCreated = new Date();
+    return { task, date, dateCreated };
+}
+
+function addTodo() {
+    const task = input.value;
+    const date = '10/10/2022';
+    const newTodo = Todo(task, date);
+    console.log(newTodo);
+    todos.push(newTodo);
+    console.log(todos);
+}
+
+button.addEventListener('click', addTodo);
